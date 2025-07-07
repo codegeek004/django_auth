e19570af6f69f46d30cd67d379b30424c27c2f7b
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-07 22:48:18 +0530

    added open id connect url using allauth library

--- project/urls.py
+++ project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
     path('accounts/', include('allauth.urls')),
+    path("", include("allauth.idp.urls")),
 ]
