48a207717ce7888636eabb79346205f8ad393a97
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-14 23:08:23 +0530

    added oidc_login, callback and oidc_logout function urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,5 +4,8 @@ urlpatterns = [
             path('', index, name='index_view'),
             path('register/', register_view, name='register'),
             path('user_login/', user_login, name='user_login'),
-            path('logout/', logout_view, name='logout_view')
+            path('logout/', logout_view, name='logout_view'),
+            path('login/oid/', oidc_login, name='oidc_login'),
+            path('logout/oidc/', oidc_logout, name='oidc_logout'),
+            path("callback", callback, name="callback"),
         ]
