c78dbc5b64c8193864f1e0d1733512be12ad48c3
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-14 23:07:35 +0530

    Added oidc login, callback and logout functions and oauth configurations. Also added context field in index function which passes the user authentication details using session

--- authentication/views.py
+++ authentication/views.py
@@ -3,9 +3,18 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 # from django.contrib.auth.models import User  
 from .models import CustomUser
+import json
+from authlib.integrations.django_client import OAuth    
+from django.conf import settings
+from django.urls import reverse
+from urllib.parse import quote_plus, urlencode
 
 def index(request):
-    return render(request, 'index.html')
+    context={
+            "session": request.session.get("user"),
+            "pretty": json.dumps(request.session.get("user"), indent=4),
+        }
+    return render(request, 'index.html', context)
 
 def register_view(request):
     print('inside register view')
@@ -52,3 +61,41 @@ def logout_view(request):
     logout(request)
     request.session.flush()
     return render(request, 'index.html')
+
+
+
+oauth = OAuth()
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
+
+
+def oidc_login(request):
+    return oauth.auth0.authorize_redirect(
+        request, request.build_absolute_uri(reverse("callback"))
+    )
+
+def callback(request):
+    token = oauth.auth0.authorize_access_token(request)
+    request.session["user"] = token
+    return redirect(request.build_absolute_uri(reverse("index")))
+
+def oidc_logout(request):
+    request.session.clear()
+
+    return redirect(
+        f"https://{settings.AUTH0_DOMAIN}/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": request.build_absolute_uri(reverse("index")),
+                "client_id": settings.AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        ),
+    )
