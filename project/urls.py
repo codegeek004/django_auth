23c5c90dd0e8a11d5b8b0e487dfc31e3483c786a
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-14 15:37:20 +0530

    removed allauth urls

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-    path('accounts/', include('allauth.urls')),
-    path("", include("allauth.idp.urls")),
 ]
