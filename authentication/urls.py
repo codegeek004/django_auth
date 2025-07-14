8efb38e3ce48697d46e2cd18df79ba895a6e41c1
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-14 17:41:49 +0530

    added register, login and logout views for authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
             path('', index, name='index_view'),
+            path('register/', register_view, name='register'),
+            path('user_login/', user_login, name='user_login'),
+            path('logout/', logout_view, name='logout_view')
         ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.models import User  
+from .models import CustomUser
 
 def index(request):
     return render(request, 'index.html')
 
+def register_view(request):
+    print('inside register view')
+    if request.method == 'POST':
+        print('in post method')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        contact = request.POST.get('contact')
+        print(f'username, {username}, password, {password}, contact, {contact}')
 
+        if CustomUser.objects.filter(username=username).exists():
+            print('user exists')
+            messages.error(request, 'username already taken')
+        else:
+            print('inside else')
+            user = CustomUser.objects.create(
+                    username=username,
+                    contact=contact
+                )
+            print('user',user)
+            user.set_password(password)
+            user.save()
+            messages.info(request, 'user created successfully')
+            print('created successflly')
+            return redirect('user_login')
+    return render(request, 'register.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if not CustomUser.objects.filter(username=username).exists():
+            messages.error(request, 'Username not found. Please register first')
+        user = authenticate(username=username, password=password)
+        print(user, 'checking activity')
+        if user is None:
+            messages.error(request, 'Invalid username or password')
+        else:
+            login(request, user)
+            return render(request, 'index.html')
+    return render(request, 'basic_login.html')
+
+def logout_view(request):
+    logout(request)
+    request.session.flush()
+    return render(request, 'index.html')
