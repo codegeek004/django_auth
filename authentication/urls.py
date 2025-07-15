bbcfc4d9a6b1844f0773481c9247335d22eedc2f
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-15 16:44:54 +0530

    changed name from index_view to index in index path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-            path('', index, name='index_view'),
+            path('', index, name='index'),
             path('register/', register_view, name='register'),
             path('user_login/', user_login, name='user_login'),
             path('logout/', logout_view, name='logout_view'),
